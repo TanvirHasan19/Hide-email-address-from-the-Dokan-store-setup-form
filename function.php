cdb0d1de672964730c2fe87fad812f661b572104
TanvirHasan19 <54507026+TanvirHasan19@users.noreply.github.com>
2022-07-13 05:56:08 +0600

    Create function.php

---
+++ function.php
@@ -0,0 +1,14 @@
+//hide email address
+function vendor_setup_wizard_styles(){
+  ?>
+  <style>
+  	
+	.wc-setup .wc-setup-content table tr:last-child{
+	display:none!important;
+	}
+  	 
+  </style>
+  <?php
+};
+
+add_action( 'dokan_setup_wizard_styles', 'vendor_setup_wizard_styles', 30 );
